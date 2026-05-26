b42c28f3e1cd3d071b44cb88193bc9aa84824032
Bruce Skingle <github@skingle.org>
2026-05-26 14:14:45 +0100

    clean and tidy

--- examples/devkitv1/src/main.rs
+++ examples/devkitv1/src/main.rs
@@ -2,21 +2,7 @@ use sparko_esp_idf::{
     binary_clock_feature::BinaryClockFeature, dyndns2::DynDns2, sparko_esp32_std::SparkoEsp32Std,
 };
 
-fn main() {
-    log::info!("Hello, world!");
-
-    // This is the app level fault barrier.
-    // For the moment we just unwrap and panic, but in the future we might want to attempt some sort of recovery or restart.
-    match run() {
-        Ok(()) => log::info!("Application finished successfully"),
-        Err(e) => {
-            log::error!("Application failed with error: {}", e);
-            panic!("App failed");
-        }
-    }
-}
-
-fn run() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     let (builder, remainder) = SparkoEsp32Std::builder()?;
 
     let smart_leds = sparko_esp_idf::smart_led::new(
@@ -31,6 +17,5 @@ fn run() -> anyhow::Result<()> {
         .with_feature(Box::new(BinaryClockFeature::new_spi(smart_leds)))?
         .build()?;
 
-    log::info!("Trace 2");
     sparko_esp32.start()
 }
